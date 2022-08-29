36d800623db19abf1a398c0f08fd5134c92d1ecf
Igor27F <Igor.2703@hotmail.com>
2022-08-29 09:26:10 -0300

    snake growing

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+let promise = navigator.mediaDevices.getUserMedia({ audio: true });
+promise
+  .then(function (signal) {
+    startRecognition();
+    animate();
+  })
+  .catch(function (err) {
+    alert("Microphone error: " + err);
+  });
 const recognitionSvc =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new recognitionSvc();
@@ -11,91 +20,137 @@ recognition.continuous = true;
 recognition.interimResults = true;
 recognition.maxAlternatives = 3;
 
+const mapWidth = 40;
+const mapHeight = 20;
 const pixelSize = 30;
-canvas.width = pixelSize * 40;
-canvas.height = pixelSize * 20;
-let snakeSpeed = 500;
-
-var snakeDirection = "right";
+canvas.width = pixelSize * mapWidth;
+canvas.height = pixelSize * mapHeight;
+let snakeSpeed = 300;
 
 function startRecognition() {
   // listening ? recognition.stop() : recognition.start();
   // listening = !listening;
   recognition.start();
 }
-var headX = canvas.width / 2 - pixelSize;
-var headY = canvas.height / 2 - pixelSize;
-animate();
 
+var square = {
+  x: 0,
+  y: 0,
+  direction: "right",
+};
+var food = {
+  x: Math.floor(Math.random() * mapWidth) * pixelSize,
+  y: Math.floor(Math.random() * mapHeight) * pixelSize,
+};
+var snake = [
+  {
+    x: 0,
+    y: 0,
+    direction: "right",
+  },
+];
+for (i = 0; i < snake.length; i++) {
+  snake[i].x = canvas.width / 2 - pixelSize * (1 + i);
+  snake[i].y = canvas.height / 2 - pixelSize;
+}
 function animate(time) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  switch (snakeDirection) {
-    case "up":
-      headY -= pixelSize;
-      break;
-    case "right":
-      headX += pixelSize;
-      break;
-    case "down":
-      headY += pixelSize;
-      break;
-    case "left":
-      headX -= pixelSize;
-      break;
-    case "stop":
-      break;
+  if (snake[0].x == food.x && snake[0].y == food.y) {
+    AddSize();
+    food.x = Math.floor(Math.random() * mapWidth) * pixelSize;
+    food.y = Math.floor(Math.random() * mapHeight) * pixelSize;
   }
-  if (headX == canvas.width) {
-    headX = 0;
-  } else if (headX == -pixelSize) {
-    headX = canvas.width - pixelSize;
+  ctx.fillStyle = "red";
+  ctx.fillRect(food.x, food.y, pixelSize, pixelSize);
+  for (i = 0; i < snake.length; i++) {
+    pixel = snake[i];
+    switch (pixel.direction) {
+      case "up":
+        pixel.y -= pixelSize;
+        break;
+      case "right":
+        pixel.x += pixelSize;
+        break;
+      case "down":
+        pixel.y += pixelSize;
+        break;
+      case "left":
+        pixel.x -= pixelSize;
+        break;
+      case "stop":
+        break;
+    }
+    if (pixel.x == canvas.width) {
+      pixel.x = 0;
+    } else if (pixel.x == -pixelSize) {
+      pixel.x = canvas.width - pixelSize;
+    }
+    if (pixel.y == canvas.height) {
+      pixel.y = 0;
+    } else if (pixel.y == -pixelSize) {
+      pixel.y = canvas.height - pixelSize;
+    }
+    ctx.fillStyle = "white";
+    ctx.fillRect(pixel.x, pixel.y, pixelSize, pixelSize);
   }
-  if (headY == canvas.height) {
-    headY = 0;
-  } else if (headY == -pixelSize) {
-    headY = canvas.height - pixelSize;
+  if (snake.length > 0) {
+    for (i = snake.length - 1; i > 0; i--) {
+      snake[i].direction = snake[i - 1].direction;
+    }
   }
-  ctx.fillStyle = "white";
-  ctx.fillRect(headX, headY, pixelSize, pixelSize);
   //ctx.lineDashOffset = time / 10000;
   //requestAnimationFrame(animate);
   setTimeout(animate, snakeSpeed);
 }
+function AddSize() {
+  snake.push({
+    x: snake[snake.length - 1].x,
+    y: snake[snake.length - 1].y,
+    direction: null,
+  });
+}
+function draw() {}
 recognition.onresult = (event) => {
   result = event.results[event.results.length - 1];
   for (i = 0; i < result.length; i++) {
-    word = result[i].transcript.toUpperCase();
-    console.log(word);
-    if (word.includes("CIMA")) {
-      snakeDirection = "up";
-    } else if (word.includes("DIREITA")) {
-      snakeDirection = "right";
-    } else if (word.includes("ESQUERDA")) {
-      snakeDirection = "left";
-    } else if (word.includes("BAIXO")) {
-      snakeDirection = "down";
-    } else if (
-      word.includes("PARA") ||
-      word.includes("CHEGA") ||
-      word.includes("PAROU")
-    ) {
-      snakeDirection = "stop";
-    }
-    if (
-      word.includes("ACELERA") ||
-      word.includes("RAPIDO") ||
-      word.includes("RÁPIDO")
-    ) {
-      snakeSpeed = 100;
-    }
-    if (
-      word.includes("DEVAGAR") ||
-      word.includes("LENTO") ||
-      word.includes("LENTA") ||
-      word.includes("DESACELERA") ||
-      word.includes("CALMA")
-    ) {
-      snakeSpeed = 500;
+    words = result[i].transcript.toUpperCase().split(" ");
+    for (j = 0; j < words.length; j++) {
+      word = words[j];
+      console.log(word);
+      if (
+        word == "CIMA" ||
+        word == "CYMA" ||
+        word == "ENCIMA" ||
+        word == "ACIMA"
+      ) {
+        snake[0].direction = "up";
+      } else if (word == "DIREITA" || word == "DIREITO") {
+        snake[0].direction = "right";
+      } else if (word == "ESQUERDA" || word == "ESQUERDO") {
+        snake[0].direction = "left";
+      } else if (
+        word == "BAIXO" ||
+        word == "BAIXA" ||
+        word == "EMBAIXO" ||
+        word == "ABAIXO" ||
+        word == "ABAIXA"
+      ) {
+        snake[0].direction = "down";
+      } else if (word == "PARA" || word == "CHEGA" || word == "PAROU") {
+        snake[0].direction = "stop";
+      }
+      if (word == "ACELERA" || word == "RAPIDO" || word == "RÁPIDO") {
+        snakeSpeed = 100;
+      }
+      if (
+        word == "DEVAGAR" ||
+        word == "LENTO" ||
+        word == "LENTA" ||
+        word == "DESACELERA" ||
+        word == "CALMA"
+      ) {
+        snakeSpeed = 500;
+      }
     }
   }
 };
